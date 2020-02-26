b28fa01790c1216789e5773827c1fd3ed9c4e294
zashii <hideki.ishizashi@gmail.com>
2020-02-27 08:21:17 +0900

    change button

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 document.getElementById("text-button").onclick = function() {
   document.getElementById("text").innerHTML = "クリックされた！";
 };
+
+document.getElementById("AAA").onclick = function() {
+  document.getElementById("text").innerHTML = "クリックされた！";
+};
