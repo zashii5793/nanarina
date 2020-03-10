3e3b9cf4457098ffc4fd59b4aa18a77f332747a5
zashii <hideki.ishizashi@gmail.com>
2020-03-11 08:12:33 +0900

    change main.js

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ document.getElementById("text-button").onclick = function() {
   document.getElementById("text").innerHTML = "クリックされた！";
 };
 
-document.getElementById("AAA").onclick = function() {
+document.getElementById("btn-blue").onclick = function() {
   alert("Hello");
-  document.getElementById("text").innerHTML = "クリックされた！";
+  //document.getElementById("text").innerHTML = "クリックされた！";
 };
+
+function btn-blueClick(){
+  alert('ボタンがクリックされました');
+}
